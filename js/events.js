1295906b0219376364d2846b0dbac70ed123716c
Andrew Simmons <neon.duncan@gmail.com>
2021-02-06 18:53:42 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,9 +1,14 @@
 //define functions here
 
 $(document).ready(function(){
-
+  getIt()
 // call functions here
 
 });
 
 
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+  })
+}
